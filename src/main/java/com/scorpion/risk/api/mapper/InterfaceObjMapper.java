c2ba8332c633a0f076eb9babb289965578837568
Jarven <769558579@qq.com>
2017-09-18 11:19:43 +0800

    risk config

--- src/main/java/com/scorpion/risk/api/mapper/InterfaceObjMapper.java
+++ src/main/java/com/scorpion/risk/api/mapper/InterfaceObjMapper.java
@@ -1,10 +1,52 @@
 package com.scorpion.risk.api.mapper;
 
+import com.github.pagehelper.Page;
+import com.scorpion.risk.api.entity.InterfaceObj;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 /**
  * Created on 2017/9/18.
  */
 @Mapper
 public interface InterfaceObjMapper {
+
+    /**
+     * 创建接口列表
+     *
+     * @param interfaceObj
+     * @return
+     */
+    int add(InterfaceObj interfaceObj);
+
+    /**
+     * 根据主键删除接口信息
+     *
+     * @param id
+     * @return
+     */
+    int deleteByPrimaryKey(Long id);
+
+    /**
+     * 修改接口信息
+     *
+     * @param interfaceObj
+     * @return
+     */
+    int update(InterfaceObj interfaceObj);
+
+    /**
+     * 分页查询
+     *
+     * @return
+     */
+    Page<InterfaceObj> findByPage();
+
+    /**
+     * 查询所有
+     *
+     * @return
+     */
+    List<InterfaceObj> findAll();
 }
--- src/main/java/com/scorpion/risk/api/service/impl/InterfaceObjServiceImpl.java
+++ src/main/java/com/scorpion/risk/api/service/impl/InterfaceObjServiceImpl.java
@@ -1,38 +1,94 @@
 package com.scorpion.risk.api.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.scorpion.risk.api.entity.InterfaceObj;
+import com.scorpion.risk.api.mapper.InterfaceObjMapper;
 import com.scorpion.risk.api.service.InterfaceObjService;
 import com.scorpion.risk.result.BaseResult;
 import com.scorpion.risk.result.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created on 2017/9/18.
  */
 @Service
 public class InterfaceObjServiceImpl implements InterfaceObjService {
+
+    @Autowired
+    private InterfaceObjMapper interfaceObjMapper;
+
+    /**
+     * 创建接口
+     *
+     * @param interfaceObj
+     * @return
+     */
     @Override
     public BaseResult add(InterfaceObj interfaceObj) {
-        return null;
+        int result = interfaceObjMapper.add(interfaceObj);
+        if (result <= 0)
+            return BaseResult.error("add_fail", "操作失败");
+        return BaseResult.success("操作成功");
     }
 
+
+    /**
+     * 根据主键删除接口信息
+     *
+     * @param id
+     * @return
+     */
     @Override
     public BaseResult deleteByPrimaryKey(Long id) {
-        return null;
+        int result = interfaceObjMapper.deleteByPrimaryKey(id);
+        if (result <= 0)
+            return BaseResult.error("delete_fail", "操作失败");
+        return BaseResult.success("操作成功");
     }
 
+
+    /**
+     * 修改接口信息
+     *
+     * @param interfaceObj
+     * @return
+     */
     @Override
     public BaseResult update(InterfaceObj interfaceObj) {
-        return null;
+        int result = interfaceObjMapper.update(interfaceObj);
+        if (result <= 0)
+            return BaseResult.error("update_fail", "操作失败");
+        return BaseResult.success("操作成功");
     }
 
+    /**
+     * 分页查询接口列表
+     *
+     * @param pageNo
+     * @param pageSize
+     * @return
+     */
     @Override
     public PageResult findByPage(Integer pageNo, Integer pageSize) {
-        return null;
+        PageHelper.startPage(pageNo, pageSize);
+        Page<InterfaceObj> page = interfaceObjMapper.findByPage();
+        PageResult result = new PageResult(page);
+        return result;
     }
 
+
+    /**
+     * 查询所有接口列表
+     *
+     * @return
+     */
     @Override
     public BaseResult findAll() {
-        return null;
+        List<InterfaceObj> list = interfaceObjMapper.findAll();
+        return BaseResult.success(list);
     }
 }
